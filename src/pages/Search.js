b3ec8e82e7a989448592ca01a01589156d4e047a
Lenon Nascimento Cardoso <lenoninformatica@gmail.com>
2022-11-04 14:46:51 -0300

    🚧Requisito 5 feito

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,43 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+    };
+  }
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+
   render() {
+    const { name } = this.state;
+    const NUMBER_MAGIC = 2;
+    const valid = name.length < NUMBER_MAGIC;
     return (
       <div data-testid="page-search">
         <h1>SEARCH🔍</h1>
         <Header />
+        <label htmlFor="input-band">
+          <input
+            id="input-login"
+            name="name"
+            type="text"
+            placeholder="Banda ou Artista"
+            data-testid="search-artist-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          onClick=""
+          disabled={ valid }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
