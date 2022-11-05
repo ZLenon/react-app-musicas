4f00b87b5a04f0d430fe57b1e8ebe9941e28e607
Lenon Nascimento Cardoso <lenoninformatica@gmail.com>
2022-11-04 22:07:19 -0300

    Requisito 6 feito🚧

---
+++ src/components/NotingAlbum.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class NotingAlbum extends React.Component {
+  render() {
+    return (
+      <h1>Nenhum álbum foi encontrado</h1>
+    );
+  }
+}
+export default NotingAlbum;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import NotingAlbum from '../components/NotingAlbum';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
       name: '',
+      saveName: '',
+      loadingAPI: false,
+      api: [],
     };
   }
 
@@ -13,32 +20,67 @@ class Search extends React.Component {
     this.setState({ [name]: value });
   };
 
-  render() {
+  handleClick = async () => {
     const { name } = this.state;
+    this.setState({ loadingAPI: true });
+    const returnAPI = await searchAlbumsAPI(name);
+    this.setState({ loadingAPI: false, saveName: name, api: returnAPI, name: '' });
+  };
+
+  render() {
+    const { name, loadingAPI, api, saveName } = this.state;
     const NUMBER_MAGIC = 2;
     const valid = name.length < NUMBER_MAGIC;
     return (
       <div data-testid="page-search">
         <h1>SEARCH🔍</h1>
         <Header />
-        <label htmlFor="input-band">
-          <input
-            id="input-login"
-            name="name"
-            type="text"
-            placeholder="Banda ou Artista"
-            data-testid="search-artist-input"
-            onChange={ this.handleChange }
-          />
-        </label>
-        <button
-          type="button"
-          data-testid="search-artist-button"
-          onClick=""
-          disabled={ valid }
-        >
-          Pesquisar
-        </button>
+        {loadingAPI ? <Loading />
+          : (
+            <>
+              <label htmlFor="input-band">
+                <input
+                  id="input-login"
+                  name="name"
+                  type="text"
+                  placeholder="Banda ou Artista"
+                  data-testid="search-artist-input"
+                  onChange={ this.handleChange }
+                />
+              </label>
+              <button
+                type="button"
+                data-testid="search-artist-button"
+                onClick={ this.handleClick }
+                disabled={ valid }
+              >
+                Pesquisar
+              </button>
+              <div>
+                {`Resultado de álbuns de: ${saveName}`}
+              </div>
+              {api.length === 0 ? <NotingAlbum />
+                : (api.map((album) => (
+                  <Link
+                    key={ album.artistId }
+                    to={ `/album/${album.collectionId}` }
+                    data-testid={ `link-to-album-${album.collectionId}` }
+                  >
+                    <img src={ album.artworkUrl100 } alt={ album.collectionId } />
+                    <div>
+                      Coleção :
+                      {album.collectionName}
+                    </div>
+                    <div>
+                      Artista :
+                      {album.artistName}
+                    </div>
+                  </Link>
+
+                ))
+                )}
+            </>
+          )}
       </div>
     );
   }
