acc711e8b789652671d4df26f60485e1b71fa40b
Lenon Nascimento Cardoso <lenoninformatica@gmail.com>
2022-11-07 23:14:46 -0300

    Arrumando algumas coisas pos mentoria

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 class MusicCard extends React.Component {
   render() {
-    const { apiSounds } = this.props;
+    const { trackName, collectionName, previewUrl, trackId, artworkUrl60 } = this.props;
     return (
-      { apiSounds.map((sound) => (
-        <div key={ sound.trackId }>
-          <div data-testid="artist-name">{sound.artistName}</div>
-          <div data-testid="album-name">{sound.collectionName}</div>
-          <audio data-testid="audio-component" src="{previewUrl}" controls>
-            <track kind="captions" />
-            O seu navegador não suporta o elemento
-            <code>audio</code>
-          </audio>
-        </div>
-      ))}
+      <div key={ trackId }>
+        <div data-testid="artist-name">{ trackName }</div>
+        <div data-testid="album-name">{ collectionName }</div>
+        <img src={ artworkUrl60 } alt={ trackId } />
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          <code>audio</code>
+        </audio>
+      </div>
+
     );
   }
 }
+
+MusicCard.propTypes = {
+  trackName: PropTypes.string,
+  collectionName: PropTypes.string,
+  previewUrl: PropTypes.string,
+  trackId: PropTypes.string,
+  artworkUrl60: PropTypes.string,
+}.isRequired;
+
 export default MusicCard;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
 import getMusics from '../services/musicsAPI';
 
 class Album extends React.Component {
@@ -15,20 +17,37 @@ class Album extends React.Component {
   }
 
   loadMusic = async () => {
-    const { match: { params: { id } } } = this.props; /* No componetes Album do navegador da para ver isso */
+    const { match: { params: { id } } } = this.props;
     const returnAPI = await getMusics(id);
-    // console.log(returnAPI);
-    returnAPI.shift(); /* Remove o primeiro item da lista de obj api */
+    returnAPI.splice(0, 1);
+    console.log(returnAPI);
     this.setState({ apiSounds: returnAPI });
   };
 
   render() {
+    const { apiSounds } = this.state;
     return (
       <div data-testid="page-album">
         <h1>🗃️ ALBUM 🔊</h1>
         <Header />
+        {
+          apiSounds.map((sound) => (
+            <MusicCard
+              key={ sound.trackId }
+              artistName={ sound.trackName }
+              collectionName={ sound.collectionName }
+              artworkUrl60={ sound.artworkUrl60 }
+              previewUrl={ sound.previewUrl }
+            />
+          ))
+        }
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  id: PropTypes.string,
+}.isRequired;
+
 export default Album;
