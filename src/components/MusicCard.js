ebb42152b0d5f2b7abd0ed8927a93afab42d26e9
Lenon Nascimento Cardoso <lenoninformatica@gmail.com>
2022-11-09 12:21:45 -0300

    Requisito 7 feito🚧

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -3,29 +3,22 @@ import PropTypes from 'prop-types';
 
 class MusicCard extends React.Component {
   render() {
-    const { trackName, collectionName, previewUrl, trackId, artworkUrl60 } = this.props;
+    const { previewUrl, trackName } = this.props;
     return (
-      <div key={ trackId }>
-        <div data-testid="artist-name">{ trackName }</div>
-        <div data-testid="album-name">{ collectionName }</div>
-        <img src={ artworkUrl60 } alt={ trackId } />
+      <>
+        <div>{trackName}</div>
         <audio data-testid="audio-component" src={ previewUrl } controls>
           <track kind="captions" />
           O seu navegador não suporta o elemento
           <code>audio</code>
         </audio>
-      </div>
-
+      </>
     );
   }
 }
 
 MusicCard.propTypes = {
   trackName: PropTypes.string,
-  collectionName: PropTypes.string,
-  previewUrl: PropTypes.string,
-  trackId: PropTypes.string,
-  artworkUrl60: PropTypes.string,
 }.isRequired;
 
 export default MusicCard;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import MusicCard from '../components/MusicCard';
 import getMusics from '../services/musicsAPI';
+import Loading from '../components/Loading';
 
 class Album extends React.Component {
   constructor() {
     super();
     this.state = {
       apiSounds: [],
+      firstINFO: {},
     };
   }
 
@@ -19,28 +21,34 @@ class Album extends React.Component {
   loadMusic = async () => {
     const { match: { params: { id } } } = this.props;
     const returnAPI = await getMusics(id);
-    returnAPI.splice(0, 1);
-    console.log(returnAPI);
-    this.setState({ apiSounds: returnAPI });
+    const spredAPI = [...returnAPI]; // espalha os objetos
+    const shifAPI = spredAPI.shift();// retorna um objeto, so o primeiro
+    // const spliceAPI = spredAPI.splice(0, 1);// retorna um array com 1 objeto, somente o primeiro
+    // console.log(returnAPI);
+    // console.log(spredAPI);
+    // console.log(shifAPI);
+    // console.log(spliceAPI);
+    this.setState({ apiSounds: spredAPI, firstINFO: shifAPI });
   };
 
   render() {
-    const { apiSounds } = this.state;
+    const { apiSounds, firstINFO } = this.state;
     return (
       <div data-testid="page-album">
         <h1>🗃️ ALBUM 🔊</h1>
         <Header />
+        <h2 data-testid="artist-name">{ firstINFO.artistName}</h2>
+        <h3 data-testid="album-name">{firstINFO.collectionName}</h3>
         {
           apiSounds.map((sound) => (
             <MusicCard
               key={ sound.trackId }
-              artistName={ sound.trackName }
-              collectionName={ sound.collectionName }
-              artworkUrl60={ sound.artworkUrl60 }
+              trackName={ sound.trackName }
               previewUrl={ sound.previewUrl }
             />
           ))
         }
+        )
       </div>
     );
   }
